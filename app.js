934be380df896f35d496701607635b72cd10ce25
Het710 <hetparikh0710@gmail.com>
2026-05-04 13:09:34 +0530

    esociety backend

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const alertRoutes=require('./src/routes/alertRoutes')
 const razorpayRoutes=require('./src/routes/razorpayRoutes')
 const discussionRoutes=require('./src/routes/discussionRoutes')
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
