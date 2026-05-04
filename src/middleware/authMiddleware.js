47c5f774a46d122d8d097f56ebf1791a0316ccee
Het710 <hetparikh0710@gmail.com>
2026-05-04 16:21:03 +0530

    esociety backend

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-const secret = 'secret';
+require('dotenv').config()
+const secret = process.env.JWT_SECRET
 
 const validateToken = async (req, res, next) => {
     try {
